5ae8f4e48d19b457fc45202a3aa60992b251a13e
flodebarre <florence.debarre@normalesup.org>
2020-07-21 17:15:47 +0200

    abstract downloaded and cleaned

--- code/downloadAbstracts.R
+++ code/downloadAbstracts.R
@@ -35,12 +35,11 @@ dlAbs <- function(DOI){
 }
 
 # Do it for all articles
-for(i in rev(3859:7032#1:nrow(allArticles)
-             )){ 
-  if(missingAbs[i]){
+for(i in xi){#rev(1:nrow(allArticles))){ 
+#  if(missingAbs[i]){
     allArticles[i, "Abstract"] <- paste0(dlAbs(allArticles[i, "DOI"]), collapse = "_")
     print(allArticles[i, "Abstract"])
-  }
+#  }
   cat("This was", i, "\n")
 }
 
@@ -48,3 +47,41 @@ for(i in rev(3859:7032#1:nrow(allArticles)
 save(allArticles, file="saveDL.RData")
 write.csv(allArticles, file = "AmNat_allAbs.csv")
 
+# Clean the downloaded abstracts
+nCA <- nchar(allArticles$Abstract)
+h <- hist(nCA, breaks = seq(0, max(nCA), by = 1))
+
+v <- allArticles$Abstract
+
+# Remove "abstracts" that were instead code to display the 1st page
+for(i in seq_along(v)){
+  if(grepl("Free first page", v[i])){
+    v[i] <- NA
+  }else{
+    # and replace empty abstracts by NA
+    if(nchar(v[i]) < 2){
+      v[i] <- NA
+    }
+  }
+}
+
+# Remove code surrounding abstract, when the abstract was downloaded
+xi <- c()
+for(i in seq_along(v)){
+  if(grepl("<!--", v[i])){
+    tmp1 <- gsub('                    <!-- abstract content --><div class=\"hlFld-Abstract\"><a name=\"abstract\"></a><div class=\"sectionInfo abstractSectionHeading\"><div class=\"sectionHeading\">Abstract</div></div><div class=\"abstractSection abstractInFull\"><p class=\"first last\">', "", v[i])
+    tmp2 <- gsub('</p></div></div><!-- /abstract content -->', "", tmp1)
+    v[i] <- tmp2
+  }
+  if(grepl("<!--", v[i])){
+    xi <- c(xi, i)
+  }
+}
+
+xi
+length(xi)
+# I will change these manually in the output file.  
+
+# Save the output
+allArticles <- cbind(allArticles, Abs = v)
+write.csv(allArticles, file = "AmNat_allAbs.csv")
